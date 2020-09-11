2466911cdbe814fffb971de6225b4f7a0d7de32c
zargabrite <38265150+zargabrite@users.noreply.github.com>
2020-09-11 17:25:10 +1200

    update server.js
    updated to reflect new, non-mouse related variables

--- server.js
+++ server.js
@@ -18,18 +18,25 @@ io.sockets.on('connection', newConnection);
 function newConnection(socket) {
     console.log('new connection: ' + socket.id);
     
-    //recieve the message 'mouse' from the client, the content of which is called with the function mouseMsg
-    socket.on('mouse', mouseMsg);
+    //recieve the message 'knobState' from the leader, the content of which is called with the function knobMsg
+    socket.on('knobState', knobMsg);
 
-    //mouseMsg function is called, which holds the data from the 'mouse' message 
-    function mouseMsg(data) {
-        //when the 'mouse' message comes in, send the data back out to the client (in this case with the same name and data)
-        socket.broadcast.emit('mouse',data);
+    //knobMsg function is called, which holds the data from the 'mouse' message 
+    function knobMsg(data) {
+        //when the 'knobState' message comes in, send the data out to the client as 'remoteKnob'
+        socket.broadcast.emit('remoteKnob',data);
         //if you wanted the data to go back out to everyone including yourself you would write
-        //io.sockets.emit('mouse',data);
+        //io.sockets.emit('remoteKnob',data);
 
+        //in the server console (terminal), print the data that was sent from the leader
+        console.log("Knob State: ", data);
+    }
 
-        //in the server console (terminal), print the data that was sent from the client
-        console.log(data);
+    socket.on('slideState', slideMsg);
+    
+    function slideMsg(data) {
+      socket.broadcast.emit('LEDstate', data);
+
+      console.log("LED State: ", data);
     }
 }
