6b38f0d9bbe1739debc1f242edd7b7a4a2336ea4
zargabrite <38265150+zargabrite@users.noreply.github.com>
2020-09-11 16:12:09 +1200

    leader.js draft 1

--- public/leader.js
+++ public/leader.js
@@ -1,38 +1,97 @@
+//display setup variables
+var minWidth = 600;   //set min width and height for canvas
+var minHeight = 400;
+var width, height;    // actual width and height for the sketch
+
+//arduino serial port related variables
+var serial;   // variable to hold an instance of the serialport library
+var portName = '/dev/tty.usbmodem14101';    // fill in your serial port name here
+var inArdData;   // variable to hold the input data from Arduino
+var outArdData; // for data output
+
 function setup() {
-  createCanvas(displayWidth,displayHeight);
-  background(51);
+    // display setup: set the canvas to match the window size
+    if (window.innerWidth > minWidth){
+      width = window.innerWidth;
+    } else {
+      width = minWidth;
+    }
+    if (window.innerHeight > minHeight) {
+      height = window.innerHeight;
+    } else {
+      height = minHeight;
+    }
+  
+    //display setup: set up canvas
+    createCanvas(width, height);
+    noStroke();
+    background(0);
 
-  socket = io.connect()
+    //arduino: set up communication port with arduino
+    serial = new p5.SerialPort();       // make a new instance of the serialport library
+    serial.on('list', printList);  // set a callback function for the serialport list event
+    serial.on('connected', serverConnected); // callback for connecting to the server
+    serial.on('open', portOpen);        // callback for the port opening
+    serial.on('data', serialEvent);     // callback for when new data arrives
+    serial.on('error', serialError);    // callback for errors
+    serial.on('close', portClose);      // callback for the port closing
 
-  //recieve the message called 'mouse' back from the server and setup event handler (function 'newDrawing')
-  socket.on('mouse', newDrawing);
+    serial.list();                      // list the serial ports
+    serial.open(portName);              // open a serial port
+
+
+    //connect to server
+    socket = io.connect()
+
+    //4. recieve the message called 'LEDVal' from the server and setup event handler (function 'LEDBrightness')
+    socket.on('LEDstate', LEDBrightness);
 }
 
-//function 'newDrawing' is called, and details of event handling are outlined
-function newDrawing(data) {
-  noStroke();
-  fill(255, 0, 100);
-  ellipse(data.x, data.y, 36, 36);
-}
-
-function mouseDragged() {
-  //in the console print mouseDragged x,y data
-  console.log(mouseX + ',' + mouseY);
-
-  //1. data that the message will contain, in this case mouse x,y
-  var data = {
-    x: mouseX,
-    y: mouseY
-  }
-
-  //1. send the message ('nameofmessage', data)
-  socket.emit('mouse', data);
-
-  noStroke();
-  fill(255);
-  ellipse(mouseX, mouseY, 36, 36);
+function LEDBrightness(data) {
+    //map the incoming LEDVal message's data to the variable outArdData
+    outArdData = data.val
+    serial.write(outArdData);//write that data to the arduino
 }
 
 function draw() {
+    // map knob attached to arduino's value to background brightness
+    var backgBrightness = map(inArdData, 0, 255, 0, 255);   // map input to the correct range of brightness
+    fill(backgBrightness);   // transfer the brightness to brightness of the color used for drawing
+    rect(0,0,width,height);   // left half
 
+    // grab arduino knob value and store it in the object data
+    var data = {
+        val: inArdData
+    }
+    // emit message 'knob' and its data
+    socket.emit('knobState', data);
 }
+
+// Following functions print the serial communication status to the console for debugging purposes
+function printList(portList) {
+    // portList is an array of serial port names
+    for (var i = 0; i < portList.length; i++) {
+    // Display the list the console:
+    print(i + " " + portList[i]);
+    }
+   }
+   
+   function serverConnected() {
+     print('connected to server.');
+   }
+   
+   function portOpen() {
+     print('the serial port opened.')
+   }
+   //read the serial data from the arduino and set the variable inArdData to equal it
+   function serialEvent() {
+     inArdData = Number(serial.read());
+   }
+   
+   function serialError(err) {
+     print('Something went wrong with the serial port. ' + err);
+   }
+   
+   function portClose() {
+     print('The serial port closed.');
+   }
