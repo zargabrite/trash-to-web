914c4651d3e93c2874e97bb1b70e90bf7c896141
zargabrite <38265150+zargabrite@users.noreply.github.com>
2020-09-15 11:51:39 +1200

    text colour handling updated

--- public/follow.js
+++ public/follow.js
@@ -1,10 +1,12 @@
+const { text } = require("body-parser");
+
 var minWidth = 600;   //set min width and height for canvas
 var minHeight = 400;
 var width, height;    // actual width and height for the sketch
 
 var slider;
 
-var backgBrightness;
+var remoteKnob = 0;
 
 function setup() {
     // set the canvas to match the window size
@@ -33,29 +35,31 @@ function setup() {
     const socket = io();
 
     //lead-to-follow: 4. listen for 'remoteKnob' messages and setup event handler (function 'LEDBrightness')
-    socket.on('remoteKnob', backgroundBrightness);
+    io.socket.on('remoteKnob', backgroundBrightness(data));
 }
 
-function printConnection () {
+function printConnection() {
     print('connected to server.')
 }
 
 function backgroundBrightness(data) {
     //lead-to-follow: 5. map the incoming remoteKnob message's data to the variable backgBrightness
-    backgBrightness = data.val
-    print(data.val);
+    remoteKnob = data.val
+    console.log(remoteKnob)
 }
 
 function draw() {
-    fill(backgBrightness);
-    rect(width,0,width,height);
+    var backgBright = map(remoteKnob, 0, 255, 0, 255);
+    fill(backgBright);
+    rect(0,0,width,height);
 
     // draw the text
-    var textRColor = map(backgBrightness, 0, 255, 255,0);
-    fill(textRColor);
-    textSize(16);
-    text("REMOTE KNOB VALUE: " + backgBrightness, width - 170, 50);
-
+    var sliderValue = slider.value()
+    var textColour = map(backgBright, 0, 255, 255,0);
+    fill(textColour);
+    textSize(18);
+    text("REMOTE KNOB VALUE: " + backgBright, 30, 50);
+    text("VIRTUAL SLIDER VALUE: " + sliderValue, 30, 20);
     // grab arduino knob value and store it in the object data
     var data = {
         //!!!this may need to be map()'d to properly control LED brightness.!!!
