6d27b4968aade8dc6c9f77c93b11fb753e5df09d
zargabrite <38265150+zargabrite@users.noreply.github.com>
2020-09-14 16:01:21 +1200

    printed 'hello' instead

--- public/leader.js
+++ public/leader.js
@@ -65,7 +65,7 @@ function draw() {
     }
     // lead-to-follow: 1. emit message 'knob' and its data
     socket.emit('knobState', data);
-    print(inArdData);
+    print('hello');
 }
 
 // Following functions print the serial communication status to the console for debugging purposes
