47db6ed92229ca704339347f4eece992e5ed95d1
zargabrite <38265150+zargabrite@users.noreply.github.com>
2020-09-25 21:50:22 +1200

    got rid of object again

--- public/leader.js
+++ public/leader.js
@@ -7,10 +7,7 @@ var width, height;    // actual width and height for the sketch
 var serial;   // variable to hold an instance of the serialport library
 var portName = 'COM3';    // fill in your serial port name here
 var inArdData;   // variable to hold the input data from Arduino
-var outArdData = {
-    p10: 0,
-    p11: 0
-}    ; // for data output
+var outArdData; // for data output
 
 var slider;
 var sliderValue;
@@ -64,9 +61,9 @@ function setup() {
 
 function LEDBrightness(data) {
     //follow-to-lead: 5. map the incoming LEDVal message's data to the variable outArdData.p10
-    outArdData.p10 = data.val
-    console.log(outArdData.p10 + "," + outArdData.p11);
-    serial.write(outArdData.p10);//write that data to the arduino
+    outArdData = data.val
+    console.log(outArdData);
+    serial.write(outArdData);//write that data to the arduino
 }
 
 function draw() {
