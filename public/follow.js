58512e1a4c38218bf8aa79e84ab014468dc96e22
zargabrite <38265150+zargabrite@users.noreply.github.com>
2020-09-29 15:46:29 +1300

    + 2nd slider

--- public/follow.js
+++ public/follow.js
@@ -4,7 +4,8 @@ var width, height;    // actual width and height for the sketch
 
 var socket;
 
-var slider;
+var slider1;
+var slider2;
 
 var remoteKnob = 0;
 
@@ -26,10 +27,15 @@ function setup() {
     noStroke();
     background(0);
     
-    //setup slider stuff
-    slider = createSlider(0, 255, 0); // indicate the value range for slider
-    slider.position(width/2 + (width/2-300)/2 , height-100);
-    slider.style('width', '300px');
+    //setup slider1 stuff
+    slider1 = createSlider(0, 255, 0); // indicate the value range for slider
+    slider1.position(width/2 + (width/2-300)/2 , height-100);
+    slider1.style('width', '300px');
+    
+    //setup slider1 stuff
+    slider2 = createSlider(0, 255, 0); // indicate the value range for slider
+    slider2.position(width/2 + (width/2-300)/2 , height-120);
+    slider2.style('width', '300px');
     
     //connect to server
     socket = io();
@@ -55,16 +61,19 @@ function draw() {
     rect(0,0,width,height);
 
     // draw the text
-    var sliderValue = slider.value()
+    var slider1Value = slider1.value();
+    var slider2Value = slider2.value();
     var textColour = map(backgBright, 0, 255, 255,0);
     fill(textColour);
     textSize(18);
-    text("REMOTE KNOB VALUE: " + backgBright, 30, 50);
-    text("VIRTUAL SLIDER VALUE: " + sliderValue, 30, 20);
+    text("VIRTUAL SLIDER 1 VALUE: " + slider1Value, 30, 20);
+    text("VIRTUAL SLIDER 2 VALUE: " + slider2Value, 30, 50);
+    text("REMOTE KNOB VALUE: " + backgBright, 30, 80);
     // grab arduino knob value and store it in the object data
     var data = {
         //!!!this may need to be map()'d to properly control LED brightness.!!!
-        val: sliderValue
+        val1: slider1Value
+        val2: slider2Value
     }
     // lead-to-follow: 1. emit message 'knob' and its data
     socket.emit('slideState', data);
